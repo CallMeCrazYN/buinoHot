83233a9c3785f5cf501038a4238489eb32574290
Niggo <nicolaskautt01@gmail.com>
2023-07-01 16:20:32 +0200

    init repo

---
+++ buinoHot.ino
@@ -0,0 +1,48 @@
+#include <Gamebuino-Meta.h>
+
+
+
+void setup() {
+  gb.begin();
+
+  
+  float pos[2];
+  float dir[2];
+
+  pos[0]=0;
+  pos[1]=0;
+  
+  dir[0]=1;
+  dir[1]=1;
+
+}    
+
+
+void loop() {
+  while(!gb.update());
+  gb.display.clear();
+
+  gb.lights.clear();
+ 
+  
+  
+  //what does dis do??
+  gb.lights.drawPixel(0,0,RED);
+
+
+  //oyy we got blue pixel
+  gb.display.setColor(BLUE);
+  gb.display.drawPixel(pos[0],pos[1]);
+
+  move(int []pos,int []dir);
+
+}
+
+
+void move(int []pos,int [] dir){
+
+  
+
+}
+
+}
